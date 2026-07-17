f6f1570a4528f0fe54dc17678cc7c9ab287d3d39
sarobidysolphi <sarobidy600@gmail.com>
2026-07-17 22:29:29 +0300

    Affichage du message Toastr dans la page

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -3,6 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Affichage des messages Toastr -->
+@if(session('success'))
+    <script>
+        toastr.success("{{ session('success') }}");
+    </script>
+@endif
+
+@if(session('error'))
+    <script>
+        toastr.error("{{ session('error') }}");
+    </script>
+@endif
     <title>Plateforme de gestion d'emploi du temps</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
