ab0025fa353488032ce36944257582fd8817146f
sarobidysolphi <sarobidy600@gmail.com>
2026-07-17 22:01:34 +0300

    Ajout de Toast.js au Layout

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Plateforme de gestion d'emploi du temps</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <!-- Toastr CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <style>
         * { margin: 0; padding: 0; box-sizing: border-box; }
